f2699d5ce337d0973bf1a42e86c8b3f96d49f44b
Helen <helenhe.mit@gmail.com>
2017-11-20 21:59:20 -0500

    muebot content search

--- muebot.py
+++ muebot.py
@@ -1,26 +1,51 @@
 import praw
+import re
 
 from datetime import datetime, timedelta
 
+keywords = {'stila': [(0,10),set(),{'eyeliner'}]}
+
+def search(text,before,after,key):
+    word = r"\W*([\w]+)"
+    res = re.search(r'{}\W*{}{}'.format(word*before,key,word*after), text)
+    if res:
+        groups = res.group().split()
+        return set(groups[:before]),set(groups[before:])
+    else:
+        return set(), set()
+
 def parse_body(body):
-    allwords = body.split()
-    for
+    for key, item in keywords.items():
+        before = item[0][0]
+        after = item[0][1]
+        before, after = search(body.lower(), before, after, key)
+        keywords_af = item[2]
+        if keywords_af:
+            for word in after:
+                if word in keywords_af:
+                    return True
+        return False
 
 def check_recents():
     reddit = praw.Reddit('bot1')
     subreddit = reddit.subreddit('makeupexchange')
 
     recents = []
-    for submission in subreddit.new(limit=10):
+    for submission in subreddit.new(limit=15):
         time = datetime.fromtimestamp(submission.created_utc)
         delta = datetime.now()-time
         if delta < timedelta(minutes=60):
             recents.append(submission)
 
+    potential = []
     for submission in recents:
         url = submission.url
         body = submission.selftext
-        parse_body(body)
+        if parse_body(body):
+            potential.append(url)
         comments = submission.comments.list()
+
+def notify():
+    pass
 
 check_recents()
